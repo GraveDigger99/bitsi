a2276ffdb6f1aca439276839817b2e6875fdc934
Ibrahimov Aydin <80528765+GraveDigger99@users.noreply.github.com>
2024-02-29 19:41:08 +0400

    Add files via upload

--- files/sendmail/sendmail.php
+++ files/sendmail/sendmail.php
@@ -54,17 +54,17 @@
 
 
 
-	//Прикрепить файл
-	if (!empty($_FILES['image']['tmp_name'])) {
-		//путь загрузки файла
-		$filePath = __DIR__ . "/files/sendmail/attachments/" . $_FILES['image']['name']; 
-		//грузим файл
-		if (copy($_FILES['image']['tmp_name'], $filePath)){
-			$fileAttach = $filePath;
-			$body.='<p><strong>Фото в приложении</strong>';
-			$mail->addAttachment($fileAttach);
-		}
-	}
+	// //Прикрепить файл
+	// if (!empty($_FILES['image']['tmp_name'])) {
+	// 	//путь загрузки файла
+	// 	$filePath = __DIR__ . "/files/sendmail/attachments/" . $_FILES['image']['name']; 
+	// 	//грузим файл
+	// 	if (copy($_FILES['image']['tmp_name'], $filePath)){
+	// 		$fileAttach = $filePath;
+	// 		$body.='<p><strong>Фото в приложении</strong>';
+	// 		$mail->addAttachment($fileAttach);
+	// 	}
+	// }
 
 	$mail->Body = $body;
 
